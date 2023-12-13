86e6b43f3613f8cac2fa3e4d33b354b17a4f56f7
mraflya1204 <150163424+mraflya1204@users.noreply.github.com>
2023-12-13 21:00:23 +0700

    Update PDIP.c

--- Practicum Finale/PDIP.c
+++ Practicum Finale/PDIP.c
@@ -1,31 +1,54 @@
 //made by Muhammad Rafly Abdillah
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-	int N, H; scanf("%d%d", &N, &H);
-	int weights[N];
-	
-	for(int i = 0; i < N; i++){
-		scanf("%d", &weights[i]);
-	}
-	
-	int totalWeight = 0;
-	for(int i = 0; i < N; i++){
-		totalWeight += weights[i];
-	}
-	
-	double tempWeight = totalWeight;
-	double tempTime = H;
-	
-	double energyNeeded = ceil(tempWeight/tempTime);
-	
-	for(int i = 0; i < N; i++){
-		if((int)energyNeeded < weights[i]){
-			energyNeeded = weights[i];
-		}
-	}
-	printf("%d", (int)energyNeeded);
+int energyCount(int *w, int n, int energy){
+    int days = 0;
+    int currentWeight = 0;
+    for(int i = 0; i < n; i++){
+        if(currentWeight + w[i] <= energy){
+            currentWeight += w[i];
+        } 
+        else{
+            days++;
+            currentWeight =w[i];
+        }
+    }
+    if(currentWeight > 0){
+        days++;
+    }
+
+    return days;
 }
 
+int binarySearch(int *w, int n, int h, int left, int right) {
+  if (left == right) {
+    return left;
+  }
 
+  int mid = left + (right - left) / 2;
+
+  if (mid < left || mid > right) {
+    return -1;
+  }
+
+  int days = energyCount(w, n, mid);
+
+  if (days > h) {
+    return binarySearch(w, n, h, mid + 1, right);
+  } else {
+    return binarySearch(w, n, h, left, mid);
+  }
+}
+
+int main() {
+  int n, h; scanf("%d %d", &n, &h);
+  int w[n];
+
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &w[i]);
+  }
+
+  int result = binarySearch(w, n, h, 0, n);
+
+  printf("%d", result);
+}
